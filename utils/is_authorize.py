a9662d4d38b2b92cd6e9d17a2d1b3e95ece326b4
vincent <119781833+songo-04@users.noreply.github.com>
2024-09-28 11:09:29 +0300

    Update is_authorize.py

--- utils/is_authorize.py
+++ utils/is_authorize.py
@@ -3,5 +3,5 @@ from fastapi import HTTPException,status
 def is_authorize(current_user_id: str,token_id: str):
     if(current_user_id != token_id):
         raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,detail='access denied'
+            status_code=status.HTTP_401_UNAUTHORIZED,detail='access denied!!'
         )
